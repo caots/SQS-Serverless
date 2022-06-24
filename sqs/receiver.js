22f3e75f2af0bff932d43f9725b28ed30ce02d95
caots <cao.transy@powergatesoftware.com>
2022-06-24 16:10:38 +0700

    update

--- sqs/receiver.js
+++ sqs/receiver.js
@@ -15,3 +15,43 @@ const consumer = async (event) => {
 };
 
 module.exports = { consumer };
+
+// var AWS = require('aws-sdk');
+// // Set the region
+// AWS.config.update({region: 'REGION'});
+
+// // Create an SQS service object
+// var sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+
+// var queueURL = "SQS_QUEUE_URL";
+
+// var params = {
+//  AttributeNames: [
+//     "SentTimestamp"
+//  ],
+//  MaxNumberOfMessages: 10,
+//  MessageAttributeNames: [
+//     "All"
+//  ],
+//  QueueUrl: queueURL,
+//  VisibilityTimeout: 20,
+//  WaitTimeSeconds: 0
+// };
+
+// sqs.receiveMessage(params, function(err, data) {
+//   if (err) {
+//     console.log("Receive Error", err);
+//   } else if (data.Messages) {
+//     var deleteParams = {
+//       QueueUrl: queueURL,
+//       ReceiptHandle: data.Messages[0].ReceiptHandle
+//     };
+//     sqs.deleteMessage(deleteParams, function(err, data) {
+//       if (err) {
+//         console.log("Delete Error", err);
+//       } else {
+//         console.log("Message Deleted", data);
+//       }
+//     });
+//   }
+// });
